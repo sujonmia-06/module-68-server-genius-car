b728b6dec75b1acc953e0b51049c425a63c303fb
sujon-mia <sujonmia.0612@gmail.com>
2022-05-01 10:57:33 +0600

    done

--- __server_deploy.js
+++ __server_deploy.js
@@ -22,6 +22,9 @@
  * 2. make sure you: git add . git commit git push
  * 3. git push heroku main
  * --------------------------------------------------------
- * connect server with client
- * 
+ * connect server with client and deploy
+ * ------------------------------------------------------
+ * 1. replace localhost by heroku link
+ * 2. npm run build
+ * 3. firebase deploy
  */
